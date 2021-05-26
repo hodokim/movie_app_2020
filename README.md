2ef419a12be2d253622390a672827e4947998504
HodoHodo <57591576+hodokim@users.noreply.github.com>
2021-05-26 23:41:30 +0900

    React study Day 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-#Movie App 2020
-React JS Fundamentals Course (2019 Update!)
+# Movie App 2020
+
+React  JS Fundamentals Course (2019 Update!)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types"
+
+function Food({name,pic,rating}){
+  return (
+  <div>
+    <h2>난 좋아~ {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={pic}/>
+  </div>
+  );
+}
+
+Food.propTypes = {
+  name:PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HI GUYS</h1>
+      {foodILike.map((dish) => (
+        <Food 
+        key={dish.id} 
+        name={dish.name} 
+        image={dish.image} 
+        rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
+
+
+  const foodILike = [
+    {
+      id: 1,
+      name: "Kimchi",
+      image:
+        "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+        rating:0.1,
+    },
+    {
+      id: 2,
+      name: "bulgogi",
+      image:
+        "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+      rating:3.5,
+    },
+    {
+      id: 3,
+      name: "kimbap",
+      image:
+        "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+      rating:3.8,
+      },
+    {
+      id: 4,
+      name: "samgyetang",
+      image:
+        "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+      rating:3.3,
+      },
+  ];
+
 
 export default App;
